74970a99e7b1724fa7bb8beb19867404835602b1
vanshitarora13-bit <vanshitarora13@gmail.com>
2026-05-26 18:18:07 +0530

    Arrays - Max Consecutive Ones

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,25 @@
+public class MaxConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+            
+            if(count > max) {
+                max = count;
+            }
+        }
+        
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 1, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+}
