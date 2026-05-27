3410187a60f99c54311a6d823d2713fbe683d1c7
vanshitarora13-bit <vanshitarora13@gmail.com>
2026-05-27 18:58:22 +0530

    Arrays - Sort Colors

---
+++ SortColors.java
@@ -0,0 +1,33 @@
+public class SortColors {
+    public static void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 2, 1, 1, 0};
+        sortColors(arr);
+        
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
